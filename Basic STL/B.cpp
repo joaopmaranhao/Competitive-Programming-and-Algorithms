8d6fa74cda6c4e0e0b31fef6651a3aee224ae090
Joao Pedro <blvck.joao37@gmail.com>
2026-05-15 13:07:26 -0300

    Initial commit

---
+++ Basic STL/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+using ll = long long;
+using llu = unsigned long long;
+
+int main(){
+        int n;
+        ll temp;
+        vector<ll> seq;
+        vector<ll> freq;
+
+        cin >> n;
+
+        for(int i = 0; i < n; i++){
+                cin >> temp;
+                seq.pb(temp);
+        }
+
+        for(int i = 0; i < n; i++){
+                freq[]
+        }
+}
