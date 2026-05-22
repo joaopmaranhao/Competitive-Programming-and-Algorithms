dc7c6d84807a094a8c8c272a7903e9bf0de440b5
Joao Pedro <blvck.joao37@gmail.com>
2026-05-22 20:45:11 -0300

    feat - questão do robin hood

--- BIT/robin_hood.cpp
+++ BIT/robin_hood.cpp
@@ -6,74 +6,72 @@
 
 using namespace std;
 
-using ll = long long;
-using llu = unsigned long long;
+typedef long long ll;
+typedef unsigned long long llu;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-//
 
 struct BIT {
-        int n;
-        vector<ll> t;
+	int n;
+	vector<ll> t;
 
-        BIT(int n) : n(n) {
-                t.assign(n + 1, 0);
-        }
-        BIT(vector<ll> const &a) {
-                n = a.size();
-                t.assign(n + 1, 0);
-                for (int i = 1; i <= n; ++i) {
-                        t[i] += a[i - 1];
-                        int j = i + (i & -i);
-                        if (j <= n) t[j] += t[i];
-                }
-        }
+	BIT(int n) : n(n) {
+		t.assign(n + 1, 0);
+	}
+	BIT(vector<ll> const &a) {
+		n = a.size();
+		t.assign(n + 1, 0);
+		for (int i = 1; i <= n; ++i) {
+			t[i] += a[i - 1];
+			int j = i + (i & -i);
+			if (j <= n) t[j] += t[i];
+		}
+	}
 
-        ll query(int i) {
-                ll ret = 0;
-                for (++i; i > 0; i -= i & -i) ret += t[i];
-                return ret;
-        }
-        ll query(int l, int r) {
-                return query(r) - query(l - 1);
-        }
+	ll query(int i) {
+		ll ret = 0;
+		for (++i; i > 0; i -= i & -i) ret += t[i];
+		return ret;
+	}
+	ll query(int l, int r) {
+		return query(r) - query(l - 1);
+	}
 
-        void update(int i, ll add) {
-                for (++i; i <= n; i += i & -i) t[i] += add;
-        }
+	void update(int i, ll add) {
+		for (++i; i <= n; i += i & -i) t[i] += add;
+	}
 };
 
-int main(){
-
-        int T, n, q;
-        vector<ll> vec;
-        vector<tuple<int, int, int>> cmds;
+int main() { _
+    int t, n, q;
+    cin >> t;
+    
+    for (int c = 1; c <= t; c++) {
         
-        cin >> T;
+        cout << "Case " << c << ":" << endl;
+        cin >> n >> q;
+        vector<ll> vec(n);
         
-        for(int i = 0; i < T; i++){
-                
-                cin >> n >> q;
-                for(int i = 0; i < n; i++){
-                        cin >> vec[i];
-                }for(int i = 0; i < q; i++){
-                        cin >> get<1>(cmds[i]);
-                        cin >> get<2>(cmds[i]);
-                        cin >> get<3>(cmds[i]);
-                }
-
-                BIT bit(vec);
-
-                for(int i = 0; i < q; i++){
-                       if(get<1>(cmds[i]) == 1){
-                                bit.update(get<2>(cmds[i]), bit.query(get<2>(cmds[i])));
-                       }else if(get<1>(cmds[i]) == 1){
-                                bit.update(get<2>(cmds[i]), get<3>(cmds[i]));
-                       }else if(get<1>(cmds[i]) == 3){
-                                bit.query(get<2>(cmds[i]), get<3>(cmds[i]));
-                       }
-                }
-
+        for (int i = 0; i < n; i++) cin >> vec[i];
+        
+        BIT bit(vec);
+        
+        while (q--) {
+            int type; cin >> type;
+            if (type == 1) {
+                int i; cin >> i;
+                ll val = bit.query(i, i);
+                cout << val << endl;
+                bit.update(i, -val);
+            } else if (type == 2) {
+                int i; ll v; cin >> i >> v;
+                bit.update(i, v);
+            } else {
+                int i, j; cin >> i >> j;
+                cout << bit.query(i, j) << endl;
+            }
         }
+    }
+    return 0;
 }
