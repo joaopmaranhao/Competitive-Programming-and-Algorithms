572492890bb23bc58d27bf146b21f5ac19745986
Joao Pedro <blvck.joao37@gmail.com>
2026-05-28 15:50:23 -0300

    feat - more questions solved
    Maps and sets are cool asf

--- stl_adv/gacha.cpp
+++ stl_adv/gacha.cpp
@@ -2,7 +2,6 @@
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
 #define pb push_back
-#define pf push_front
 #define all(x) (x).begin(), (x).end()
 
 using namespace std;
@@ -16,27 +15,18 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 int main(){ _
  
         int n;
+        set<string> items;
+
         cin >> n;
 
-        deque<ll> deq;
         for(int i = 0; i < n; i++) {
-                ll temp;
+                string temp;
                 cin >> temp;
-
-                (i % 2  == 0) ? deq.pb(temp) :  deq.pf(temp);
+                
+                items.insert(temp);
         }
 
+        cout << items.size() << endl;
         
-        if(n % 2 == 0) {
-                for(ll x : deq) {
-                        cout << x << " ";
-                }
-        }
-        else {
-                for(int i = n - 1; i >= 0; i--){
-                        cout << deq[i] << " ";
-                }
-        }
-        cout << endl;
         return 0;
 }
--- stl_adv/ohana.cpp
+++ stl_adv/ohana.cpp
@@ -2,7 +2,6 @@
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
 #define pb push_back
-#define pf push_front
 #define all(x) (x).begin(), (x).end()
 
 using namespace std;
@@ -15,28 +14,26 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 int main(){ _
  
+        
         int n;
         cin >> n;
+  
+        map<string, ll> room;
 
-        deque<ll> deq;
         for(int i = 0; i < n; i++) {
-                ll temp;
+                string temp;
                 cin >> temp;
-
-                (i % 2  == 0) ? deq.pb(temp) :  deq.pf(temp);
+                room[temp]++;
         }
 
-        
-        if(n % 2 == 0) {
-                for(ll x : deq) {
-                        cout << x << " ";
+
+        ll max_count = 0;
+        for(auto const& [name, count] : room) {
+                if(count > max_count) {
+                        max_count = count;
                 }
         }
-        else {
-                for(int i = n - 1; i >= 0; i--){
-                        cout << deq[i] << " ";
-                }
-        }
-        cout << endl;
+
+        cout << max_count << endl;
         return 0;
 }
