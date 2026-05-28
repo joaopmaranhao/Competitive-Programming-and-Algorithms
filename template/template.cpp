7d040925ee7a90cfbf19d05dd8ab3e91a3749efe
Joao Pedro <blvck.joao37@gmail.com>
2026-05-28 13:27:12 -0300

    feat - some questions solved

---
+++ template/template.cpp
@@ -0,0 +1,20 @@
+//Credits to @GabrielCFormiga for the template
+
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main(){ _
+    
+    return 0;
+}
