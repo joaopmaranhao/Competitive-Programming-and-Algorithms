aacd665d398f8abc0a5198a8d05126b8152bf017
Joao Pedro <blvck.joao37@gmail.com>
2026-06-20 11:39:42 -0300

    feat: line question solved

---
+++ number_theory/line.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+ll exgcd(ll a, ll b, ll& x, ll& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    ll d = exgcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+int main(){
+    
+    ll a, b, c;
+    cin >> a >> b >> c;
+    c = -c;
+    ll x, y;
+    
+    int p = exgcd(a, b, x, y);
+    int k = c / p;
+    if(c % p == 0) cout << x * k << " " << y * k << endl;
+    else cout << "-1" << endl;
+}
