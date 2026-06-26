0dd889e527658f43c716b9eaf699394c41ce62cf
JoaoPedro <blvck.joao37@gmail.com>
2026-06-26 14:30:19 -0300

    feat: commentary on bacteria.cpp

--- bitmask/bacteria.cpp
+++ bitmask/bacteria.cpp
@@ -14,6 +14,15 @@ using uint32 = unsigned int;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+/*
+        Well, this needs more explaining than 4 lines of code, the idea
+        behind the algorithm is simple, each day multiplies the number of bacteria by 2,
+        so it is basically a left shift, since you can oly put one bacteria in a day
+        each bit can only be inserted in the first position, what we want is to find
+        the number of days we have to put a bacteria in tthe box, wich by the structure of the problem
+        you can deduce it is the number of bits set as 1 in the binary representation of the number
+*/
+
 int main(){ _
 
         uint32 n;
@@ -21,6 +30,6 @@ int main(){ _
         bitset<32> b(n);
 
         cout << b.count() << endl;
-        
+
         return 0;
 }
