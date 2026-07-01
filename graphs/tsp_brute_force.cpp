1db553c441fb8daf0c39f438c6203a2329cadb7b
JoaoPedro <blvck.joao37@gmail.com>
2026-07-01 13:04:40 -0300

    feat:  TSP backtrack lol

--- graphs/tsp_brute_force.cpp
+++ graphs/tsp_brute_force.cpp
@@ -21,51 +21,60 @@ struct Point {
         Point() = default;
         Point(string l, float x, float y) : label_(l), x_(x), y_(y) {}
 
-        Point operator-(const Point& other) const {
-                return {x_ - other.x_, y_ - other.y_};
-        }
+        Point operator-(const Point& other) const { return Point("", x_ - other.x_, y_ - other.y_); }
+        float dot(const Point& other) const { return x_ * other.x_ + y_ * other.y_; }
 
-        float dot(const Point& other) const {
-                return x_ * other.x_ + y_ * other.y_;
-        }
-
-        float norm() const {
-                return std::sqrt(this->dot(*this));
-        }
+        float norm() const { return std::sqrt(this->dot(*this)); }
 };
-
-float distance(Point a, Point b){
-        return (a - b).norm();
-}
+float distance(Point a, Point b){ return (a - b).norm(); }
 
 struct solution {
+        // Path used
         vector<Point> path;
 
+        // Cost function of the path
         float cost(){
+                if(path.empty()) return 1e9;
+
                 float c = 0;
-                for(int i = 1; i < path.size() - 1; i++){
+                for(int i = 0; i < path.size() - 1; i++){
                         c += distance(path[i], path[i+1]);
                 }
+                c += distance(path.back(), path.front());
+                return c;
         }
 };
 
 
 vector<Point> cities;
+vector<bool> visited;
 solution curr_path;
 solution ans;
+float min_cost = 1e9;
 
-void backtrack(){
+void backtrack(int u){
         //base case
         if(curr_path.path.size() == cities.size()){
-                if(curr_path.cost() < ans.cost()){
+                float curr_cost = curr_path.cost();
+                if(curr_cost < min_cost){
+                        min_cost = curr_cost;
                         ans = curr_path;
-                        return;
-                }else{
-                        return;
                 }
+                return;
         }
 
-        //
+        for(int v = 0; v < cities.size(); v++){
+                if(!visited[v]){
+                        //make choice
+                        visited[v] = true;
+                        curr_path.path.pb(cities[v]);
+                        //recursive call
+                        backtrack(v);
+                        //undo choice
+                        curr_path.path.pop_back();
+                        visited[v] = false;
+                }
+        }
 }
 
 int main(){ _
@@ -74,21 +83,25 @@ int main(){ _
         cin >> n;
 
         for(int i = 0; i < n; i++){
-                int x, y;
                 string label;
-                cin >> x >> y >> label; 
+                int x, y;
+                cin >> label >> x >> y; 
 
                 Point p(label, x, y);
                 cities.pb(p);
         }
 
-        backtrack();
+        visited.assign(n, false);
+        visited[0] = true;
+        curr_path.path.pb(cities[0]);
 
-        cout << "Solução: [";
-        for(auto& v : ans){
-                cout << v << " ";
+        backtrack(0);
+
+        cout << "Solução de custo " << min_cost << ": [";
+        for(int i = 0; i < ans.path.size(); i++){
+                cout << ans.path[i].label_ << (i == ans.path.size() - 1 ? "" : " -> ");
         }
-        cout << "]" << endl;
+        cout << " -> " << ans.path.front().label_ << "]" << endl;
     
         return 0;
 }
