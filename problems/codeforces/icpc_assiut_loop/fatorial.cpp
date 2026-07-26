153e3584ad6c2b7405abf3f1d204d2c452db1cce
JoaoPedro <blvck.joao37@gmail.com>
2026-07-25 21:56:03 -0300

    feat - on fire more

---
+++ problems/codeforces/icpc_assiut_loop/fatorial.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define endl '\n'
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int MOD = 1e9 + 7;
+
+int main() { _
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        ll fat = 1;
+        for(int i = 1; i <= n; i++) {
+            ll prev = fat;
+            fat = i * prev;
+        }
+        cout << fat << endl;
+    }
+
+    return 0;
+}
---
+++ problems/codeforces/icpc_assiut_loop/max.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define endl '\n'
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int MOD = 1e9 + 7;
+
+int main() { _
+
+    int n;
+    cin >> n;
+
+    vector<ll> v(n);
+
+    ll mx = -LINF;
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+        mx = max(v[i], mx);      
+    }
+    cout << mx << endl;
+
+    return 0;
+}
